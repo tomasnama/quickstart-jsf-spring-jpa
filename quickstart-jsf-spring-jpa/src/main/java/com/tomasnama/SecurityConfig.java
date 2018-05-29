03d845bd5f19ba5d6d399d24f584d56805c10d78
Tomás Navarro <tomas.navarro@gmail.com>
2018-05-29 23:37:53 +0200

    Config Spring security JSF

--- quickstart-jsf-spring-jpa/src/main/java/com/tomasnama/SecurityConfig.java
+++ quickstart-jsf-spring-jpa/src/main/java/com/tomasnama/SecurityConfig.java
@@ -1,45 +1,42 @@
 package com.tomasnama;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@Configuration
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage("/login")
+        http.authorizeRequests()
+        		.anyRequest()
+        		.authenticated()
+        
+        .and().formLogin()
+                .loginPage("/login.xhtml")
                 .permitAll()
-                .and()
-            .logout()
-                .permitAll();
+                .failureUrl("/login.xhtml?error=true")
+        
+        .and().logout()
+		        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		        .invalidateHttpSession(true)
+		        .logoutSuccessUrl( "/login.xhtml")
+        
+        .and().csrf().disable();
     }
 	
-	 @Bean
-	    @Override
-	    public UserDetailsService userDetailsService() {
-	        UserDetails user =
-	             User.withUsername("user")
-	                .username("user")
-	                .password("password")
-	                .roles("USER")
-	                .build();
-	        return new InMemoryUserDetailsManager(user);
-	    }
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("{noop}password").roles("ADMIN");
+		// {bcrypt}$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG
+	}
+
 
 }
